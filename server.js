8ada225a1e5bbc7a9b7fc3b5e2dce9b951750d25
Yousef <103825369+yousefshref@users.noreply.github.com>
2023-08-21 15:20:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 export const server = 'https://yoummercesystemmostafaomar.pythonanywhere.com/'
 // export const server = 'http://127.0.0.1:8000/'
 
-export const host = 'http://localhost:3000/'
+export const host = 'https://yoummerce-client-moomar.vercel.app/'
 
 
 export const Yoummerce = 'Car accessories'
