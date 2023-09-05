d89593e150b1dd1d23b5fa77bc694a890de9e39e
yousefshref <yb2005at@gmail.com>
2023-09-05 23:07:02 +0300

    d

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-// export const server = 'https://yoummercesystemmostafaomar.pythonanywhere.com/'
-export const server = 'http://127.0.0.1:8000/'
+export const server = 'https://yoummercesystemmostafaomar.pythonanywhere.com/'
+// export const server = 'http://127.0.0.1:8000/'
 
 export const host = 'https://yoummerce-client-moomar.vercel.app/'
 
